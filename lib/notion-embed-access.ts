0841282f649bec2b794ddb066be956e257503d7b
Nicodemus <nicodemusdu@gmail.com>
2022-10-25 17:16:32 +0800

    add lib

---
+++ lib/notion-embed-access.ts
@@ -0,0 +1,98 @@
+/*
+ * @Author: Nicodemus nicodemusdu@gmail.com
+ * @Date: 2022-10-25 14:40:30
+ * @LastEditors: Nicodemus nicodemusdu@gmail.com
+ * @LastEditTime: 2022-10-25 16:37:24
+ * @FilePath: /notion-statistics/lib/notion-embed-access.ts
+ * @Description: 判断当前页面是否潜入在notion中
+ *
+ * Copyright (c) 2022 by Nicodemus nicodemusdu@gmail.com, All Rights Reserved.
+ */
+export function isEmbed() {
+    return !(window.self === window.top);
+}
+
+export function isEmbedInNotion(targetUrl: string) {
+    if (isEmbed()) {
+        // const currentUrlObj = urlParse(window.location.href);
+        // const targetUrlObj = urlParse(targetUrl);
+        // if (currentUrlObj.scheme === targetUrlObj.scheme && currentUrlObj.host === targetUrlObj.host) {
+        //     return true;
+        // }
+        const currentUrl = window.location.href;
+        const currentNotionDomain = getNotionSiteDomain(currentUrl) || getNotionSoDomain(currentUrl);
+        const targetNotionDomain = getNotionSiteDomain(targetUrl) || getNotionSoDomain(targetUrl);
+        if (currentNotionDomain && targetNotionDomain && currentNotionDomain === targetNotionDomain) {
+            return true;
+        }
+        // 两种表现形式
+        // https://seedao.notion.site/5443fbba0e694306a72d5271a9f1eb34     公开页面
+        // https://www.notion.so/seedao/d4377e7963224371b9284061032bd67e   需要访问权限
+    }
+    return false;
+}
+
+interface IUrl {
+    url: string;
+    scheme: string; // http https...
+    slash: string; //  //
+    host: string;
+    port?: string;
+    path?: string;
+    query?: string;
+    hash?: string;
+}
+export function urlParse(url: string): IUrl {
+    var parse_url = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
+    const result = parse_url.exec(url) || [];
+    enum EFieldIndex {
+        URL = 0,
+        SCHEME = 1,
+        SLASH = 2,
+        HOST = 3,
+        PORT = 4,
+        PATH = 5,
+        QUERY = 6,
+        HASH = 7,
+    }
+    return {
+        url: result[EFieldIndex.URL],
+        scheme: result[EFieldIndex.SCHEME],
+        slash: result[EFieldIndex.SLASH],
+        host: result[EFieldIndex.HOST],
+        port: result[EFieldIndex.PORT],
+        path: result[EFieldIndex.PATH],
+        query: result[EFieldIndex.QUERY],
+        hash: result[EFieldIndex.HASH],
+    } as IUrl;
+}
+export function getNotionSiteDomain(ur: string): string | null {
+    const parse_url =
+        /^(?:([A-Za-z]+):)?(\/{0,3})([0-9\-A-Za-z]+).([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
+    const result = parse_url.exec(ur) || [];
+    console.log(result);
+    enum EFieldIndex {
+        URL = 0,
+        SCHEME = 1,
+        SLASH = 2,
+        DOMAIN = 3, // 用户的域名(例如: nicodemus    https://nicodemus.notion.site)
+        HOST = 4,
+    }
+
+    return result && result[EFieldIndex.HOST] === 'notion.site' ? result[EFieldIndex.DOMAIN] : null;
+}
+export function getNotionSoDomain(ur: string): string | null {
+    const parse_url =
+        /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)\/([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
+    const result = parse_url.exec(ur) || [];
+    console.log(result);
+    enum EFieldIndex {
+        URL = 0,
+        SCHEME = 1,
+        SLASH = 2,
+        HOST = 3,
+        DOMAIN = 4, // 用户的域名(例如: nicodemus    https://www.notion.so/nicodemus/)
+    }
+
+    return result && result[EFieldIndex.HOST] === 'www.notion.so' ? result[EFieldIndex.DOMAIN] : null;
+}
---
+++ pages/posts/notion.tsx
@@ -0,0 +1,46 @@
+/*
+ * @Author: Nicodemus nicodemusdu@gmail.com
+ * @Date: 2022-10-22 16:30:41
+ * @LastEditors: Nicodemus nicodemusdu@gmail.com
+ * @LastEditTime: 2022-10-25 16:39:00
+ * @FilePath: /notion-statistics/pages/posts/notion.tsx
+ * @Description:
+ *
+ * Copyright (c) 2022 by Nicodemus nicodemusdu@gmail.com, All Rights Reserved.
+ */
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { urlParse, isEmbedInNotion } from '../../lib/notion-embed-access';
+
+export default function FirstPost() {
+    const routers = useRouter();
+    useEffect(() => {
+        const url = (process.env.NEXT_PUBLIC_TARGET_URL as string) || '';
+        console.log(`target url = ${url}, is embed notion: ${isEmbedInNotion(url)} :`, process.env.NODE_ENV);
+    });
+    return (
+        <div>
+            <Head>
+                <title> Frirst Post</title>
+            </Head>
+            <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={() => {
+                    console.log('load script');
+                }}
+            />
+            <h1>First Post </h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+            <Image src="/images/profile.png" height={256} width={256} alt="nicodemus" />
+        </div>
+    );
+}
