04a7c27b3401fe6761639bad5cc89eeecc9e4a96
Nicodemus <nicodemusdu@gmail.com>
2022-10-25 11:30:01 +0800

    format and test vercel update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/*
+ * @Author: Nicodemus nicodemusdu@gmail.com
+ * @Date: 2022-10-22 16:16:27
+ * @LastEditors: Nicodemus nicodemusdu@gmail.com
+ * @LastEditTime: 2022-10-25 11:28:55
+ * @FilePath: /notion-statistics/pages/_app.tsx
+ * @Description:
+ *
+ * Copyright (c) 2022 by Nicodemus nicodemusdu@gmail.com, All Rights Reserved.
+ */
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,20 @@
+/*
+ * @Author: Nicodemus nicodemusdu@gmail.com
+ * @Date: 2022-10-22 16:16:27
+ * @LastEditors: Nicodemus nicodemusdu@gmail.com
+ * @LastEditTime: 2022-10-25 11:28:58
+ * @FilePath: /notion-statistics/pages/api/hello.ts
+ * @Description:
+ *
+ * Copyright (c) 2022 by Nicodemus nicodemusdu@gmail.com, All Rights Reserved.
+ */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
-}
+    name: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    res.status(200).json({ name: 'John Doe' });
 }
